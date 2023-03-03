2880efe1865634da874476b5f686ccffeec6c08d
ShengYan, Zhang <yan.code@gmail.com>
2023-03-03 17:31:40 +0800

    feat: add menu on side bar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,13 +1,87 @@
 import {FC} from "react";
-import {Box} from "@mui/material";
+import {Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch} from "@mui/material";
+import {AccountBox, Article, DarkMode, Group, Home, Person, Settings, Storefront} from "@mui/icons-material";
 
 const Sidebar: FC = () => {
     return (<Box
         flex={1}
         p={2}
         sx={{display: {xs: "none", sm: 'inline'}}}
-        bgcolor='deepskyblue'>
-        Sidebar
+    >
+
+        <Box sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
+            <nav aria-label="main mailbox folders">
+                <List>
+                    <ListItem disablePadding>
+                        <ListItemButton component="a" href="#home">
+                            <ListItemIcon>
+                                <Home/>
+                            </ListItemIcon>
+                            <ListItemText primary="Homepage"/>
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton component="a" href="#simple-list">
+                            <ListItemIcon>
+                                <Article/>
+                            </ListItemIcon>
+                            <ListItemText primary="Pages"/>
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton component="a" href="#simple-list">
+                            <ListItemIcon>
+                                <Group/>
+                            </ListItemIcon>
+                            <ListItemText primary="Groups"/>
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton component="a" href="#simple-list">
+                            <ListItemIcon>
+                                <Storefront/>
+                            </ListItemIcon>
+                            <ListItemText primary="Marketplace"/>
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton component="a" href="#simple-list">
+                            <ListItemIcon>
+                                <Person/>
+                            </ListItemIcon>
+                            <ListItemText primary="Friends"/>
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton component="a" href="#simple-list">
+                            <ListItemIcon>
+                                <Settings/>
+                            </ListItemIcon>
+                            <ListItemText primary="Settings"/>
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton component="a" href="#simple-list">
+                            <ListItemIcon>
+                                <AccountBox/>
+                            </ListItemIcon>
+                            <ListItemText primary="Profile"/>
+                        </ListItemButton>
+                    </ListItem>
+                    {/*dark mode switch*/}
+                    <ListItem disablePadding>
+                        <ListItemButton >
+                            <ListItemIcon>
+                                <DarkMode/>
+                            </ListItemIcon>
+                            <Switch/>
+                        </ListItemButton>
+
+                    </ListItem>
+                </List>
+            </nav>
+        </Box>
+
     </Box>)
 }
 export default Sidebar
